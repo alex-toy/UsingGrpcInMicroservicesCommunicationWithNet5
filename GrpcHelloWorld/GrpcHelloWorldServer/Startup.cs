b4a382c366d5f0492c6d35e45b77526360f67736
alex-toy <alexei.80@hotmail.fr>
2022-11-20 08:56:28 +0100

    hello world server

--- GrpcHelloWorld/GrpcHelloWorldServer/Startup.cs
+++ GrpcHelloWorld/GrpcHelloWorldServer/Startup.cs
@@ -1,13 +1,10 @@
+using GrpcHelloWorldServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GrpcHelloWorldServer
 {
@@ -23,7 +20,7 @@ namespace GrpcHelloWorldServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddGrpc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,23 +30,17 @@ namespace GrpcHelloWorldServer
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapGrpcService<HelloWorldService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
--- README.md
+++ README.md
@@ -6,45 +6,26 @@ In this project, we will build, develop and manage gRPC servers and clients on d
 
 
 
-
 ## GrpcHelloWorld 
 
 ### Package Manager Command in **GrpcHelloWorldServer**
 ```
-Install-Package Grpc.AspNetCore ##2.33.1 version
+Install-Package Grpc.AspNetCore
 ```
 
 ### Package Manager Command in **GrpcHelloWorldClient**
 ```
-Install-Package Grpc.Net.Client ##2.33.1 version
-Install-Package Google.Protobuf ##3.14.1 version
-Install-Package Grpc.Tools ##2.33.1 version
+Install-Package Grpc.Net.Client
+Install-Package Google.Protobuf
+Install-Package Grpc.Tools
 ```
 
-### Deployment
+<img src="/pictures/proto_config.png" title="proto config"  width="800">
 
-1. Run *Articles.API* microservice **locally**
-- Run :
+### Package Manager Command in **GrpcGreeter**
 ```
-docker start articles-mongo
+Install-Package Grpc.AspNetCore
 ```
-- In *appsettings.Development.json*, set : "ConnectionString" : "mongodb://localhost:27017"
-- Hit **Articles.API**
-
-<img src="/pictures/article_swagger.png" title="article swagger"  width="800">
-
-2. Run *Articles.API* microservice **containerized**
-- In *appsettings.Development.json*, set : "ConnectionString" : "mongodb://articlemongodb:27017"
-- Hit **Docker Compose**
-
-3. Run *Articles.API* microservice **docker compose**
-```
-docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml down
-docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml up -d
-```
-- go to *Articles.API* : http://localhost:8000/swagger/index.html
-
-<img src="/pictures/article_swagger_docker.png" title="article swagger_docker"  width="800">
 
 
 
